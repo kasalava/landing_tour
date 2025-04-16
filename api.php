2ca49d81873d37dceb96912a35bf5f20de647aa7
kasalava <kasalava86@gmail.com>
2025-04-17 01:26:22 +0500

    add pdo mysql

--- api.php
+++ api.php
@@ -1,6 +1,9 @@
 <?php
-$count = 50; //переменная
-if (count == 50) {
-    echo 'true'
 
-}
+$name = $_POST['name'];
+$tel = $_POST['phone'];
+$email = $_POST['email'];
+
+echo '<h1>name </h1>' $name
+echo '<h1>tel </h1>' $tel
+echo '<h1>email </h1>' $email
